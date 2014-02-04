006af8f4dc46d9a54fa88d5bc5161d2743ed5289
Dinesh Kesarwani <dinesh_cyberwani@yahoo.com>
2014-02-04 13:09:50 +0530

    Create image-select.js

---
+++ js/image-select.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+
+	// On label click, change the input and class
+	jQuery('.acf-image-select label img, .acf-image-select label .acf-image-select-title').click(function(e) {
+		var id         = jQuery(this).closest('label').attr('for');
+		var parentList = jQuery(this).closest('ul.acf-image-select-list');
+		
+		parentList.find('.acf-image-select-selected').removeClass('acf-image-select-selected').find("input[type='radio']").attr("checked",false).removeAttr("data-checked");
+		
+		jQuery('label[for="' + id + '"]').addClass('acf-image-select-selected').find("input[type='radio']").attr("checked",true).attr("data-checked","checked");
+		
+	});
+	
+});
+
